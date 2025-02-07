1eaaf226428752ed8aec2f50e6ad8c3b2433b69d
rohit <rohitsb96134@gmail.com>
2025-02-07 13:41:10 +0530

    add FlyonUI integration and update dependencies; configure routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
+
+
 
 function App() {
-  
+  const location = useLocation();
+
+  useEffect(() => {
+    const loadFlyonui = async () => {
+      await import('flyonui/flyonui');
+      window.HSStaticMethods.autoInit();
+    };
+    loadFlyonui();
+  }, [location.pathname]);
 
   return (
-  <>
-  <div >
-    hello</div></>
-  )
+    <> 
+      <div className='font-normal text-3xl'>hello</div>
+    </>
+  );
 }
 
-export default App
+export default App;
