6ee9dc227859aa73f777052e94c52a367f36d986
rohit <rohitsb96134@gmail.com>
2025-02-09 17:59:41 +0530

    fix: wrap application in StoreContextProvider for state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar.jsx';
+import Navbar from "./components/Navbar/Navbar.jsx";
+import StoreContextProvider from "./context/storeContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <Navbar />
-      <App />
+      <StoreContextProvider>
+        <Navbar />
+        <App />
+      </StoreContextProvider>
     </Router>
-    
   </StrictMode>
 );
