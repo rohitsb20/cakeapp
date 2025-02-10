eb630e5a8a0c4a0ad636ad9d8d95298383196264
rohit <rohitsb96134@gmail.com>
2025-02-10 14:01:48 +0530

    feat: integrate StoreContext for food items and filter by category in FoodDisplay component

--- frontend/src/components/FoodDisplay/FoodDislpay.jsx
+++ frontend/src/components/FoodDisplay/FoodDislpay.jsx
@@ -1,29 +1,41 @@
-import { foodItems } from "../../assets/asset";
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+
 import FoodCard from "../foodCard/FoodCard";
+import { StoreContext } from "../../context/storeContext";
+
+const FoodDisplay = ({ category }) => {
+  const { foodItems } = useContext(StoreContext);
+
+  return (
+    <div>
+      <h2 className="text-3xl capitalize ">top cakes </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {foodItems.map((item, index) => {
 
 
-const FoodDisplay = () => {
-
-    
-
-    return (
-      <div>
-        <h2 className="text-3xl ">top cakes </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4"> 
-          {foodItems.map((item, index) => (
-            <FoodCard 
-            key={index} 
-            id={item.id}
-             name={item.name} 
-             description={item.description}
-             price={item.price}
-             category={item.category} />
-          ))}
-        </div>
+if (category === "All" || category === item.category) {
+          return  (
+            <FoodCard
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              category={item.category}
+              image={item.image}
+            />
+          );
+        } else{
+          return null;
+        }
+        
+       
+       
+       })}
       </div>
-    );
-
-
-}
+    </div>
+  );
+};
 
 export default FoodDisplay;
