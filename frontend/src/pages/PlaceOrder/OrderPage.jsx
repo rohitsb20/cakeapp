4ce65bc542812344e837767961e9119063b4a6d3
rohit <rohitsb96134@gmail.com>
2025-02-10 21:31:04 +0530

    refactor: simplify OrderPage layout by removing CartTotal and proceed button

--- frontend/src/pages/PlaceOrder/OrderPage.jsx
+++ frontend/src/pages/PlaceOrder/OrderPage.jsx
@@ -1,38 +1,17 @@
-import OrderForm from "./OrderDetailForm/OrderForm"
-import CartTotal from "../Cart/CartTotal/CartTotal"
-import { useNavigate } from "react-router-dom";
+import OrderForm from "./OrderDetailForm/OrderForm";
 
 const OrderPage = () => {
-
-   const navigate = useNavigate();
-        const handleProceed = () => {
-          navigate("/orderpage");
-        }; 
+ 
   return (
     <div>
-      <h1 className="capitalize text-2xl font-semibold ">order Page</h1>
-
-      <div className="flex flex-col md:flex-row justify-center items-center gap-2">
-        <div className="w-full md:w-1/2">
-          <OrderForm />
-        </div>
-        <div className="divider divider-horizontal hidden md:block "></div>
-        <div className="divider  md:hidden "></div>
-        <div className="w-full md:w-1/2">
-          <CartTotal />
-          <div className="flex justify-center mt-4">
-            <button
-              onClick={handleProceed}
-              className="btn bg-pink-500 text-white
-           hover:bg-pink-600 waves waves-light"
-            >
-              Proceed to checkout
-            </button>
-          </div>
-        </div>
-      </div>
+      <h2 className="mt-6 text-center text-2xl font-normal text-pink-500">
+        Enter Details
+      </h2>
+   
+        <OrderForm />
+   
     </div>
   );
-}
+};
 
-export default OrderPage
+export default OrderPage;
