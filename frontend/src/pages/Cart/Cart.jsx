3748b9bccd721db603ef3f0182827d1af675202d
rohit <rohitsb96134@gmail.com>
2025-02-10 17:51:31 +0530

    feat: enhance Cart component with empty state handling and navigation

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,72 +1,84 @@
-//import {useNavigate} from "react-router-dom";
 import { StoreContext } from "../../context/storeContext";
 import { Trash2 } from "lucide-react";
 import { useContext } from "react";
+import { Frown } from "lucide-react";
+import {useNavigate} from 'react-router-dom';
 
-//import {Frown}  from "lucide-react"
+
 const Cart = () => {
-  const {
-    foodItems,
-    cartItems,
-    removeFromCart,
-  } = useContext(StoreContext);
-  //  const navigate = useNavigate();
-  // const handleClick = () => {
-  //   navigate("/");
-  // };
+  const navigate = useNavigate();
+  const { foodItems, cartItems, removeFromCart } = useContext(StoreContext);
+ const isEmpty = Object.keys(cartItems).length === 0;
+ const handleClick = () => {
+    navigate('/')
+ }
+
   return (
-    <div className="container min-h-screen mx-auto p-4">
-      <div>
-        <div className="capitalize font-semibold grid grid-cols-6 gap-4 items-center">
-          <p>Items</p>
-          <p>Title</p>
-          <p>price</p>
-          <p>quantity</p>
-          <p>total</p>
-          <p>remove</p>
+    <div className="container mx-auto p-4">
+      {isEmpty ? (
+        <div className="my-4 flex justify-center">
+          <div className="flex flex-col items-center gap-y-3">
+            <Frown size={150} />
+            <h1 className="text-2xl">Your cart is empty</h1>
+
+            <button
+              onClick={handleClick}
+              className="btn btn-outline capitalize
+             btn-sm rounded-full
+              border border-pink-500
+               hover:border-pink-500
+               hover:text-white
+               hover:bg-pink-500"
+            >
+              go to shopping
+            </button>
+          </div>
         </div>
-        <br />
-        <hr />
-      
-        <div >
-          {
-            foodItems.map(
-              (item) => {
-                if(cartItems[item._id]>0)
-                {
-                  return (
-                    <>
-                    
-                      <div
-                        key={item.id}
-                        className="grid grid-cols-6 gap-x-4  gap-y-8 items-center"
-                      >
-                        <img
-                          src={item.image}
-                          alt={item.name}
-                          className="w-[4rem] rounded-lg"
-                        />
-                        <p>{item.name}</p>
-                        <p className="text-lg font-medium">{item.price}</p>
-                        <p>{cartItems[item._id]}</p>
-                        <p className="text-lg font-medium">
-                          {item.price * cartItems[item._id]}
-                        </p>
-                        <p>
-                          <Trash2 
+      ) : (
+        <div>
+          <div className="capitalize font-semibold grid grid-cols-6 gap-4 items-center">
+            <p>Items</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+          </div>
+          <br />
+          <hr />
+          <div>
+            {foodItems.map((item) => {
+              if (cartItems[item._id] > 0) {
+                return (
+                  <div key={item._id}>
+                    <div className="grid grid-cols-6 gap-x-4 gap-y-8 items-center">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="bg-base-content/10 h-10 w-10 rounded-md"
+                      />
+                      <p>{item.name}</p>
+                      <p className="text-lg font-medium">{item.price}</p>
+                      <p>{cartItems[item._id]}</p>
+                      <p className="text-lg font-medium">
+                        {item.price * cartItems[item._id]}
+                      </p>
+                      <p>
+                        <Trash2
                           onClick={() => removeFromCart(item._id)}
-                           className="hover:text-red-500" />
-                        </p>
-                      </div>{" "}
-                      <hr />
-                    </>
-                  );
-                }
+                          className="hover:text-red-500"
+                        />
+                      </p>
+                    </div>
+                    <hr />
+                  </div>
+                );
               }
-            )
-          }
+              return null;
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
