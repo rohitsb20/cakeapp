eec2825cf629c98816e26880d9c9d467ccb2b476
rohit <rohitsb96134@gmail.com>
2025-02-10 14:00:38 +0530

    fix: add missing import for StoreContextProvider in main entry file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
+
 import StoreContextProvider from "./context/storeContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
