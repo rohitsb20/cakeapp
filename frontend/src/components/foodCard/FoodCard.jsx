8fe1481d40f72b7dac466d340ee9cf5f415afa1c
rohit <rohitsb96134@gmail.com>
2025-02-10 14:19:34 +0530

    feat: enhance FoodCard component with image display and improved cart interaction

--- frontend/src/components/foodCard/FoodCard.jsx
+++ frontend/src/components/foodCard/FoodCard.jsx
@@ -3,27 +3,38 @@ import { Minus, Plus } from "lucide-react";
 import { useContext } from "react";
 import { StoreContext } from "../../context/storeContext";
 
-const FoodCard = ({ id, name, price, description, category }) => {
+const FoodCard = ({ id, name, price, description, category,image }) => {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="border-2 w-fit p-2">
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>{price}</p>
-      <p>{category}</p>
+      <img src={image} alt={name} />
+
+      <h3 className="mt-2 text-xl font-semibold">{name}</h3>
+      <p className="italic text-sm">{description}</p>
+      <p className="font-bold text-2xl"> &#8377;{price}</p>
+      <p>flavour :{category}</p>
       {/*itemcount i sreplced by cart[id] */}
       {!cartItems[id] ? (
-        <div>
-          <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
+        <div >
+          
+          <button
+            className="bg-green-500 text-white px-2 py-1 rounded-lg"
+            onClick={() => addToCart(id)}
+          >
+            Add to cart
+          </button>
         </div>
       ) : (
-        <div>
-          <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
-          <span>{cartItems[id]}</span>
+        <div className="flex items-center ">
           <Minus
-            className="cursor-pointer"
+            className="cursor-pointer border-2 rounded-full"
             onClick={() => removeFromCart(id)}
+          />
+            <span className="px-2">{cartItems[id]}</span>
+          <Plus
+            className="cursor-pointer border-2 rounded-full"
+            onClick={() => addToCart(id)}
           />
         </div>
       )}
