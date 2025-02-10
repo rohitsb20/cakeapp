d66bfdb7c6aa0516840c5d86dabec74cd031cd5a
rohit <rohitsb96134@gmail.com>
2025-02-10 14:01:13 +0530

    refactor: reorganize imports and streamline FoodCard component structure

--- frontend/src/components/foodCard/FoodCard.jsx
+++ frontend/src/components/foodCard/FoodCard.jsx
@@ -1,37 +1,34 @@
 /* eslint-disable react/prop-types */
-import { useContext } from "react";
 import { Minus, Plus } from "lucide-react";
+import { useContext } from "react";
 import { StoreContext } from "../../context/storeContext";
 
 const FoodCard = ({ id, name, price, description, category }) => {
-  
-   // const [itemCount, setItemCount] = useState(0);
-  
-    const { addToCart, removeFromCart , cartItems} = useContext(StoreContext);
-  
-    return (
-      <div className="border-2 w-fit p-2">
-        <h3>{name}</h3>
-        <p>{description}</p>
-        <p>{price}</p>
-        <p>{category}</p>
-        {/*itemcount i sreplced by cart[id] */}
-        {!cartItems[id] ? (
-          <div>
-            <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
-          </div>
-        ) : (
-          <div>
-            <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
-            <span>{cartItems[id]}</span>
-            <Minus
-              className="cursor-pointer"
-              onClick={() => removeFromCart(id)}
-            />
-          </div>
-        )}
-      </div>
-    );
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="border-2 w-fit p-2">
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <p>{price}</p>
+      <p>{category}</p>
+      {/*itemcount i sreplced by cart[id] */}
+      {!cartItems[id] ? (
+        <div>
+          <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
+        </div>
+      ) : (
+        <div>
+          <Plus className="cursor-pointer" onClick={() => addToCart(id)} />
+          <span>{cartItems[id]}</span>
+          <Minus
+            className="cursor-pointer"
+            onClick={() => removeFromCart(id)}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FoodCard;
