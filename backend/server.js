27055d0aa232461b10c0fd2bc1a48306642fa510
rohit <rohitsb96134@gmail.com>
2025-02-12 20:01:58 +0530

    feat: add food routes to server for handling food-related API requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./database/dbConnect.js";
 import authRoutes from "./routes/auth.routes.js";
+import foodRoutes from "./routes/food.routes.js";
+
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,9 +11,8 @@ dotenv.config();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.use("/api/food", foodRoutes);
+
 
 app.listen(port, () => {
   dbConnect();
