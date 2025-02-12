5e9615d47eee720a57ebd5745cab7d51d4a37892
rohit <rohitsb96134@gmail.com>
2025-02-12 14:59:21 +0530

    fix: correct confirmPassword field name and improve input types in signup form

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import useSignup from "../../hooks/useSignup";
+
 const Signup = () => {
   const { signuphandler } = useSignup();
   const [formData, setFormData] = useState({
@@ -8,15 +9,14 @@ const Signup = () => {
     lastname: "",
     email: "",
     password: "",
-    confrimPassword: "",
+    confirmPassword: "",
     phone: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
-    setFormData((prev) => ({
-      ...prev,
+    setFormData((prevData) => ({
+      ...prevData,
       [name]: value,
     }));
   };
@@ -27,76 +27,60 @@ const Signup = () => {
   };
 
   return (
-    <div className=" mt-2 flex items-start justify-center bg-white p-4">
-      <div className="max-w-4xl w-full flex rounded-lg shadow-sm  overflow-hidden bg-white">
+    <div className="mt-2 flex items-start justify-center bg-white p-4">
+      <div className="max-w-4xl w-full flex rounded-lg shadow-sm overflow-hidden bg-white">
         {/* Image for large screens */}
-        <div className="hidden lg:block lg:w-1/2 ">
+        <div className="hidden lg:block lg:w-1/2">
           <img
             src="./src/assets/images/auth-image.jpg"
             alt="auth image"
             width={600}
             height={600}
-            className="object-contain w-full h-full "
+            className="object-contain w-full h-full"
           />
         </div>
 
         {/* Signup form */}
         <div className="w-full lg:w-1/2 p-8">
-          <h2
-            className="text-3xl font-bold
-           mb-6 text-center text-pink-500"
-          >
+          <h2 className="text-3xl font-bold mb-6 text-center text-pink-500">
             Signup
           </h2>
           <form onSubmit={handleSubmit} className="space-y-6">
             <div className="flex justify-between space-x-3">
-              {/* first name field */}
+              {/* First name field */}
               <div>
                 <label
                   htmlFor="firstname"
-                  className="block text-sm
-                 font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700"
                 >
                   Firstname
                 </label>
                 <input
                   id="firstname"
                   name="firstname"
-                  type="firstname"
+                  type="text"
                   required
                   value={formData.firstname}
                   onChange={handleChange}
-                  className="mt-1 block
-                 w-full px-3 py-2 border
-                  border-gray-300 rounded-md
-                   shadow-sm focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
                 />
               </div>
-              {/* last name field */}
-
+              {/* Last name field */}
               <div>
                 <label
                   htmlFor="lastname"
-                  className="block text-sm
-                 font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700"
                 >
                   Lastname
                 </label>
                 <input
                   id="lastname"
                   name="lastname"
-                  type="lastname"
+                  type="text"
                   required
                   value={formData.lastname}
                   onChange={handleChange}
-                  className="mt-1 block
-                 w-full px-3 py-2 border
-                  border-gray-300 rounded-md
-                   shadow-sm focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
                 />
               </div>
             </div>
@@ -105,24 +89,18 @@ const Signup = () => {
             <div>
               <label
                 htmlFor="phone"
-                className="block text-sm
-                 font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-700"
               >
                 Phone
               </label>
               <input
                 id="phone"
                 name="phone"
-                type="phone"
+                type="text"
                 required
                 value={formData.phone}
                 onChange={handleChange}
-                className="mt-1 block
-                 w-full px-3 py-2 border
-                  border-gray-300 rounded-md
-                   shadow-sm focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
               />
             </div>
 
@@ -130,8 +108,7 @@ const Signup = () => {
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm
-                 font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-700"
               >
                 Email
               </label>
@@ -142,12 +119,7 @@ const Signup = () => {
                 required
                 value={formData.email}
                 onChange={handleChange}
-                className="mt-1 block
-                 w-full px-3 py-2 border
-                  border-gray-300 rounded-md
-                   shadow-sm focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
               />
             </div>
 
@@ -166,14 +138,7 @@ const Signup = () => {
                 required
                 value={formData.password}
                 onChange={handleChange}
-                className="mt-1 block w-full
-                 px-3 py-2 border
-                  border-gray-300
-                   rounded-md shadow-sm
-                   
-                   focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
               />
             </div>
 
@@ -188,34 +153,17 @@ const Signup = () => {
               <input
                 id="confirmPassword"
                 name="confirmPassword"
-                type="confirmPassword"
+                type="password"
                 required
                 value={formData.confirmPassword}
                 onChange={handleChange}
-                className="mt-1 block w-full
-                 px-3 py-2 border
-                  border-gray-300
-                   rounded-md shadow-sm
-                   
-                   focus:outline-none
-                    focus:ring-pink-500
-                     focus:border-pink-500"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-pink-500 focus:border-pink-500"
               />
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full flex
-                 justify-center py-2 px-4
-                  border border-transparent
-                   rounded-md shadow-sm text-sm
-                    font-medium text-white
-                     bg-pink-500
-                      hover:bg-pink-600
-                       focus:outline-none 
-                       focus:ring-2 
-                       focus:ring-offset-2
-                        focus:ring-pink-600"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600"
               >
                 Sign Up
               </button>
@@ -224,8 +172,7 @@ const Signup = () => {
           <div className="mt-4 text-center">
             <Link
               to="/signin"
-              className="text-sm
-               text-pink-600 hover:text-pink-500"
+              className="text-sm text-pink-600 hover:text-pink-500"
             >
               Already have an account? Sign In
             </Link>
