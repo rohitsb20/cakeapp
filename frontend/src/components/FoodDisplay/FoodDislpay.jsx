a0ae53adcd9805ccabd4e0390172124fafb631ed
rohit <rohitsb96134@gmail.com>
2025-02-14 15:41:47 +0530

    feat: add no cakes available message with icon in FoodDisplay component

--- frontend/src/components/FoodDisplay/FoodDislpay.jsx
+++ frontend/src/components/FoodDisplay/FoodDislpay.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react";
+import { Frown } from "lucide-react";
 
 import FoodCard from "../foodCard/FoodCard";
 import { StoreContext } from "../../context/storeContext";
@@ -17,24 +18,33 @@ const FoodDisplay = ({ category }) => {
         top cakes{" "}
       </h2>
       <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4  mt-3">
-        {data.map((item, index) => {
-          const url = "http://localhost:4000";
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodCard
-                key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                category={item.category}
-                image={`${url}/images/${item.image}`}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {data.length === 0 ? (
+          <div className=" text-red-500 text-xl">
+            <p className="flex items-center
+             justify-center gap-x-3">
+              <span>No Cakes available</span> <Frown />
+            </p>
+          </div>
+        ) : (
+          data.map((item, index) => {
+            const url = "http://localhost:4000";
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodCard
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  category={item.category}
+                  image={`${url}/images/${item.image}`}
+                />
+              );
+            } else {
+              return null;
+            }
+          })
+        )}
       </div>
     </div>
   );
