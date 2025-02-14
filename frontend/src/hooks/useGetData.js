436ccf23afb1d7dfe6a78f10b45443e1fdd04635
rohit <rohitsb96134@gmail.com>
2025-02-14 15:42:20 +0530

    fix: added  data as dependency

--- frontend/src/hooks/useGetData.js
+++ frontend/src/hooks/useGetData.js
@@ -2,36 +2,30 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 const useGetData = () => {
-   const [data, setData] = useState([]);
-   const url = "http://localhost:4000";
+  const [data, setData] = useState([]);
+  const url = "http://localhost:4000";
 
-   const fetchData = async () => {
-     try {
-       const response = await fetch(`${url}/api/cake/list`);
-       const result = await response.json();
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${url}/api/cake/list`);
+      const result = await response.json();
 
-       if (result.success) {
-         setData(result.data);
-       
-       } else {
-         toast.error("Error in fetching data");
-       }
-     } catch (error) {
-       console.error("Error fetching data: ", error);
-       toast.error("Error in fetching data");
-     }
-   };
+      if (result.success) {
+        setData(result.data);
+      } else {
+        toast.error("Error in fetching data");
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      toast.error("Error in fetching data");
+    }
+  };
 
-   useEffect(() => {
-     fetchData();
-   }, []);
+  useEffect(() => {
+    fetchData();
+  }, [data]);
 
+  return { data };
+};
 
-     
-         
-return {data}
-}
-
-export default useGetData
-
-
+export default useGetData;
