6160e140a39b28057805f644fa94efab25a8cb27
rohit <rohitsb96134@gmail.com>
2025-02-17 13:08:35 +0530

    feat: add Order model with schema for managing orders

---
+++ backend/model/order.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "In Process",
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    payment: {
+      type: Boolean,
+     default:false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.models.order || mongoose.model("Order", orderSchema);
+
+export default Order;
