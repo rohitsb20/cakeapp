f02af0f4aa564cbe0538b19f1565ea706c6cbb18
rohit <rohitsb96134@gmail.com>
2025-02-24 12:35:18 +0530

    fix: correct import path for StoreContext in FoodDisplay component

--- frontend/src/components/FoodDisplay/FoodDislpay.jsx
+++ frontend/src/components/FoodDisplay/FoodDislpay.jsx
@@ -3,16 +3,10 @@ import { useContext } from "react";
 import { Frown } from "lucide-react";
 
 import FoodCard from "../foodCard/FoodCard";
-import { StoreContext } from "../../context/storeContext";
-
+import { StoreContext } from "../../context/StoreContext";
 
 const FoodDisplay = ({ category }) => {
   const { data } = useContext(StoreContext);
-
-  
-
-
-  
 
   return (
     <div>
@@ -22,17 +16,15 @@ const FoodDisplay = ({ category }) => {
       <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4  mt-3">
         {data.length === 0 ? (
           <div className=" text-red-500 text-xl">
-            <p className="flex items-center
-             justify-center gap-x-3">
+            <p
+              className="flex items-center
+             justify-center gap-x-3"
+            >
               <span>No Cakes available</span> <Frown />
             </p>
           </div>
         ) : (
           data.map((item, index) => {
-       console.log("item", item);
-       
-       
-            
             const url = "http://localhost:4000";
             if (category === "All" || category === item.category) {
               return (
